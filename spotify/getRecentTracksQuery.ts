6749a2adb1738ca9dc77cc8be12f26232e86a85f
Dai Nguyendo <dainguyendo@gmail.com>
2021-04-12 21:44:26 -0400

    delete: unused function

--- spotify/getRecentTracksQuery.ts
+++ spotify/getRecentTracksQuery.ts
@@ -8,9 +8,3 @@ export async function getRecentTracksQuery(limit: number = 20) {
   const data: SpotifyApi.UsersRecentlyPlayedTracksResponse = await response.json();
   return data;
 }
-
-export async function getRecentTrackQuery() {
-  const recentTracks = await getRecentTracksQuery(1);
-  const mostRecentTrack = recentTracks.items[0];
-  return mostRecentTrack;
-}
