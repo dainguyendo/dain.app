5ad6874d02a6c505aa654f4eedbf5886bef588b5
Dai Nguyendo <dainguyendo@gmail.com>
2021-10-06 09:45:59 -0400

    Migrate track related pages to use ISR. Subsequently remove react-query as dep

--- packages/spotify/getRecentTracks.ts
+++ packages/spotify/getRecentTracks.ts
@@ -4,9 +4,12 @@ export const getRecentTracks = async (limit: number = 20) => {
   const { access_token } = await getAccessToken();
   const endpoint = `https://api.spotify.com/v1/me/player/recently-played?limit=${limit}`;
 
-  return fetch(endpoint, {
+  const response = await fetch(endpoint, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   });
+
+  const data: SpotifyApi.UsersRecentlyPlayedTracksResponse = await response.json();
+  return data;
 };
